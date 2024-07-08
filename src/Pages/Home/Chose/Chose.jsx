f813a1159c056489696b15cad6d9f164be52dd1f
SharminAkter3 <mssharmin4145@gmail.com>
2024-07-09 00:12:39 +0600

    added home section

---
+++ src/Pages/Home/Chose/Chose.jsx
@@ -0,0 +1,67 @@
+import '../../../App.css';
+import choseImage1 from '../../../assets/Images/login/signup_img.jpeg';
+import choseImage2 from '../../../assets/Images/Process/choseImage2.png';
+import choseImage3 from '../../../assets/Images/Process/choseImage3.png';
+
+
+const Chose = () => {
+    return (
+        <div className='container'>
+            <div>
+                <small className='section-title'>Why Chose Us?</small>
+            </div>
+            <div>
+                <h1 className='h1-text'>
+                    <p className='text-black'>Your Partner in Smart <br /> Energy Solutions</p>
+                </h1>
+            </div>
+            <div>
+                <p className=''>Understand, Solve & Evolve <br />Powering Your World with Reliable & Sustainable Energy. <br />Affordable, cleaner and simple renewable energy solutions from a global energy partner.</p>
+            </div>
+
+            <div className='chose-card-container'>
+                <div className='chose-card'>
+                    <div>
+                        <img className='chose-image'
+                            src={choseImage1}
+                            alt="chose-image"
+                        />
+                    </div>
+                    <div className='chose-content'>
+                        <h1 className='h1-text'>Renewable Energy</h1>
+                        <p className='p-text'>Our renewable energy options are designed to be <br />100% sustainable, offering you a choice of advanced <br /> renewable energy technologies and strategic <br />locations. Each option is supported by UK-certified <br />renewable certificates, ensuring that your energy <br /> comes from verified green sources.</p>
+                    </div>
+                </div>
+
+                <div className='chose-card'>
+                    <div className='chose-content'>
+                        <h1 className='h1-text'>Affordability</h1>
+                        <p className='p-text'>We offer tailored pricing plans to fit your specific <br /> energy needs and budget. Benefit from our extensive <br /> expertise in energy efficiency to maximize your <br /> savings. Additionally, leverage the purchasing <br />strength of Shell to receive unparalleled value in your <br /> energy solutions.</p>
+                    </div>
+                    <div>
+                        <img className='chose-image'
+                            src={choseImage2}
+                            alt="chose-image"
+                        />
+                    </div>
+                </div>
+
+                <div className='chose-card'>
+                    <div>
+                        <img className='chose-image'
+                            src={choseImage3}
+                            alt="chose-image"
+                        />
+                    </div>
+                    <div className='chose-content'>
+                        <h1 className='h1-text'>Simplicity</h1>
+                        <p className='p-text'>Experience hassle-free service with our UK-based <br />customer care team, dedicated to providing prompt <br />and efficient support. Manage your energy usage <br /> effortlessly through our online customer portal, which <br /> offers flexible options for both fixed and variable <br /> pricing plans, giving you control and peace of mind.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Chose;
---
+++ src/Pages/Home/Product/Product.jsx
@@ -0,0 +1,74 @@
+import '../../../App.css';
+
+const Product = () => {
+    return (
+        <div className="container">
+            <div>
+                <small className='section-title'>Our Products</small>
+            </div>
+            <div>
+                <h1 className='h1-text'>
+                    <p className='text-black'>Discover Our Best Solutions <br />for You</p>
+                </h1>
+            </div>
+            <div>
+                <p>
+                    Discover our cutting-edge energy solutions designed for efficiency and sustainability. From advanced solar panels to <br /> innovative wind turbines and energy storage systems, [Company Name] offers reliable products to power a greener future. <br /> Lower costs, reduce your carbon footprint, and embrace energy independence with our state-of-the-art technology. <br /> Explore our range and start your journey towards sustainable energy today.
+                </p>
+            </div>
+
+            <div className='card-container'>
+                <div className="card w-96 shadow-xl no-rounded bg-[#4CAF50]">
+                    <figure className="no-rounded">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                            alt="Shoes"
+                            className="no-rounded card-image" />
+                    </figure>
+                    <div className="card-body items-center text-center no-rounded">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p className='card-description'>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="mb-3">
+                            <button className="button">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 shadow-xl no-rounded bg-[#4CAF50]">
+                    <figure className="no-rounded">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                            alt="Shoes"
+                            className="no-rounded card-image" />
+                    </figure>
+                    <div className="card-body items-center text-center no-rounded">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p className='card-description'>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="mb-3">
+                            <button className="button">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card w-96 shadow-xl no-rounded bg-[#4CAF50]">
+                    <figure className="no-rounded">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                            alt="Shoes"
+                            className="no-rounded card-image" />
+                    </figure>
+                    <div className="card-body items-center text-center no-rounded">
+                        <h2 className="card-title">Shoes!</h2>
+                        <p className='card-description'>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="mb-3">
+                            <button className="button">Learn More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <button className='see-button'>See more</button>
+
+        </div >
+    );
+};
+
+export default Product;
