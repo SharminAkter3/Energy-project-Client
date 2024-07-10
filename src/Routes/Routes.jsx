b4e8ae382a932883a267233ae017850bd70142c6
SharminAkter3 <mssharmin4145@gmail.com>
2024-07-10 19:38:09 +0600

    our-service page completed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import Invoice from "../Pages/Dashboard/Invoice/Invoice";
 import Upload from "../Pages/Dashboard/Upload/Upload";
 import About from "../Pages/About/About/About";
 import Contact from "../Pages/Contact/Contact/Contact";
+import OurService from "../Pages/OurService/OurService/OurService";
 
 export const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>,
+      },
+      {
+        path: 'our_service',
+        element: <OurService></OurService>
       },
       {
         path: 'about',
@@ -69,7 +74,7 @@ export const router = createBrowserRouter([
         element: <Upload></Upload>,
       },
 
- 
-],
+
+    ],
   },
 ]);
